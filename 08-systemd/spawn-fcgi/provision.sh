768992259666441098ba5c53cb33b3f129024a0f
alan <alan@mini.localdomain>
2019-09-04 23:32:46 +0300

    unit for spawn-fcgi

---
+++ 08-systemd/spawn-fcgi/provision.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yum install -y epel-release -y && yum install -y spawn-fcgi php php-cli mod_fcgid httpd
+cp /vagrant/spawn-fcgi.service /usr/lib/systemd/system/
+sed -i 's/#SOCKET/SOCKET/' /etc/sysconfig/spawn-fcgi
+sed -i 's/#OPTIONS/OPTIONS/' /etc/sysconfig/spawn-fcgi
+sed -i 's/-P \/var\/run\/spawn-fcgi\.pid //' /etc/sysconfig/spawn-fcgi
+systemctl enable spawn-fcgi
+systemctl start spawn-fcgi
+
